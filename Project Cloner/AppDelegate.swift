b2cb93b4b5ed3dac73d1fcb22c5f032f3bd7e0f4
dick-ey <mdickey46@gmail.com>
2017-10-03 13:09:43 -0600

    Initial Commit

---
+++ Project Cloner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project Cloner
+//
+//  Created by Matt Dickey on 10/3/17.
+//  Copyright © 2017 Matt Dickey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project Cloner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project Cloner
+//
+//  Created by Matt Dickey on 10/3/17.
+//  Copyright © 2017 Matt Dickey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
